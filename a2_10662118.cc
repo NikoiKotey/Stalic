75a217923028985a4701bff6ea6d16cf563c7e98
NikoiKotey <42815650+NikoiKotey@users.noreply.github.com>
2018-08-29 16:11:41 -0700

    Add files via upload

---
+++ a2_10662118.cc
@@ -0,0 +1,25 @@
+#include <iosstream>
+using namespace std;
+
+int main( )
+{
+
+double n, a ;
+bool prime number = true;
+ cout << “Please enter positive integer ; “;
+cin >>n;
+
+for (i=2 ; I <=n\2 ; ++i )
+ {( n% i==0)
+   {
+     Prime number = false
+      break ;
+    }
+
+if (prime number)
+cout <<” This is  a prime number “;
+else
+cout<< “This is not a prime number “
+
+return 0
+}
